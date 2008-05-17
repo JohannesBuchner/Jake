5abb66e25308243a671b5fa45fc8dc870ad781b7
simon <simon@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-05-17 15:34:29 +0000

    imlemented JakeObject, updated the class diagramm accordingly
    git-svn-id: svn://svn.jakeapp.com/trunk@253 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/doublesignal/sepm/jake/core/domain/JakeObject.java
+++ core/src/main/java/com/doublesignal/sepm/jake/core/domain/JakeObject.java
@@ -1,43 +1,66 @@
 package com.doublesignal.sepm.jake.core.domain;
 
+import java.util.LinkedList;
 import java.util.List;
-import java.io.Serializable;
 
 /**
- * SEPM SS08
- * Gruppe: 3950
- * Projekt: Jake - a collaborative Environment
- * User: johannes, domdorn
- * Date: May 9, 2008
- * Time: 00:25:03 AM
+ * This is the base of all Objects used in jake, like files, notes, etc.
+ * The <code>JakeObject</code> has a <code>name </code> and may be tagged with
+ * <code>tags</code>.
  */
-public class JakeObject implements Serializable {
+public class JakeObject {
 
     private String name;
-
-
+    private LinkedList<Tag> tags;
+    
+    /**
+     * Get the <code>name</code> of the object.
+     * @return <code>name</code>
+     */
+    public String getName() {
+    	return name;
+    }
+    
+    /**
+     * Get the tags of the object
+     * @return List of <code>tags</code> that are appended to this object.
+     */
     public List<Tag> getTags() {
-        return null;
+        return tags;
     }
 
-    public JakeObject addTag(String tag) {
-        return this;
+    /**
+     * Add a tag to the object.
+     * @param tag to be added
+     */
+    public void addTag(Tag tag) {
+        tags.add(tag);
     }
 
-    public JakeObject addTag(Tag tag) {
-        return this;
+    /**
+     * Removes all <code>tags</code> from the object that are equal to <code>tag</code>.
+     * @param tag to be removed
+     */
+    public void removeTag(Tag tag) {
+        /* I'm affright that is does not do what i want. remove() should be repeated
+         * as long as it returns true...
+         */ 
+    	while (tags.remove(tag));
     }
 
-    public JakeObject removeTag(Tag tag) {
-        return this;
-    }
-
-    public void delete() {
-        /* delete this jake object */
-    }
-
-    public boolean equals(Object o) {
-        /* TODO */
-        return super.equals(o);
+    /**
+     * Test if this object is equal to obj
+     * @param obj the object to be tested
+     * @return <code>true</code> iff the <code>name</code> and <code>tags</code>
+     * are equal.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null && this.getClass().equals(obj.getClass())) {
+        	JakeObject that = (JakeObject)obj;
+        	return (this.name.equals(that.getName()) &&
+        	        this.tags.equals(that.getTags()));
+        }
+        return false;
     }
 }
