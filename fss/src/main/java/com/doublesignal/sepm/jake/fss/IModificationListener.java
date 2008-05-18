d47a5f3608888a41a819078744c8bea332719162
dominik <dominik@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-05-18 01:44:32 +0000

    renamed to correct naming convention
    git-svn-id: svn://svn.jakeapp.com/trunk@258 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ fss/src/main/java/com/doublesignal/sepm/jake/fss/IModificationListener.java
@@ -0,0 +1,17 @@
+package com.doublesignal.sepm.jake.fss;
+
+/**
+ * Objects listening for file modifications have to implement this.
+ * 
+ * @author johannes
+ */
+
+public interface IModificationListener {
+	/**
+	 * Actions that can occur for a file on a filesystem.
+	 */
+	enum ModifyActions { CREATED, REMOVED, CHANGED }
+	
+	void fileModified(String relpath, ModifyActions action);
+	
+}
