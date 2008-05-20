0a95c7614d43ff0c1f8328a637dd507e6196bff6
dominik <dominik@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-05-20 22:15:50 +0000

    startup class for (possible) different frontends
    git-svn-id: svn://svn.jakeapp.com/trunk@299 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ core/src/main/java/com/doublesignal/sepm/jake/core/StartJake.java
@@ -0,0 +1,38 @@
+package com.doublesignal.sepm.jake.core;
+
+import com.doublesignal.sepm.jake.gui.JakeGui;
+
+
+/**
+ * SEPM SS08
+ * Gruppe: 3950
+ * Projekt: Jake - a collaborative Environment
+ * User: domdorn
+ * Date: May 20, 2008
+ * Time: 10:56:48 PM
+ */
+public class StartJake
+{
+	public static void main(String[] args)
+	{
+		System.out.println("==================================================================================");
+		System.out.println("Welcome to Jake - a collaborative Environment\n\n");
+
+		System.out.println("We're happy you're using our software.");
+		System.out.println("For any acknowledgments, gifts or what else you want to give us,\n"
+		+"feel free to send us an email! \n\n");
+
+		System.out.println("==================================================================================\n\n");
+
+
+		System.out.println("starting frontend....");
+
+		// hier kommt noch eine art getOpt() hin, mit der man per kommandozeile (theoretisch) verschiedene frontends
+		// laden koennte. auf jeden fall sollte das gui mit dependency injection geladen werden und nicht so statisch
+		// wie jetzt.
+
+		new JakeGui();
+
+		System.out.println("some other output after gui thread started");
+	}
+}
