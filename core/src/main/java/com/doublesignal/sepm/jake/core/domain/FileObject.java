752a07bab58a4bdfb33194e95614028cb6edadf4
Simon <Simon@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-05-22 12:53:57 +0000

    updated FileObject added test for the file ob
    git-svn-id: svn://svn.jakeapp.com/trunk@312 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/doublesignal/sepm/jake/core/domain/FileObject.java
+++ core/src/main/java/com/doublesignal/sepm/jake/core/domain/FileObject.java
@@ -2,26 +2,27 @@ package com.doublesignal.sepm.jake.core.domain;
 
 /**
  * The <code>FileObject</code> represents a file in the application.
- * It contains of a <code>relpath</code> that points to the file.
+ * It contains of a <code>name</code> (i.e the relpath) that points to the file.
  *
  * @author Simon
  */
 public class FileObject extends JakeObject {
 	/**
-	 * Create a new <code>FileObject</code> with a given <code>relpath</code>
+	 * Create a new <code>FileObject</code> with a given <code>name</code> i.e
+	 * relative path to the file.
 	 *
-	 * @param relpath relative path to the file
+	 * @param name relative path to the file
 	 */
-	public FileObject(String relpath) {
-		super(relpath);
+	public FileObject(String name) {
+		super(name);
 	}
 
 	/**
 	 * Get the relative path to the file.
-	 * <p/>
 	 * TODO: I left this in here in case any of you still use it. We should get rid of it ASAP.
 	 *
 	 * @return relative path
+	 * @deprecated use .getName() instead
 	 */
 	@Deprecated
 	public String getRelpath() {
@@ -31,7 +32,7 @@ public class FileObject extends JakeObject {
 	/**
 	 * Test if two <code>FileObjects</code>are equal.
 	 *
-	 * @return <code>true</code> if the two <code>relpath</code>s are equal.
+	 * @return <code>true</code> if the two <code>name</code>s are equal.
 	 */
 	@Override
 	public boolean equals(Object obj) {
