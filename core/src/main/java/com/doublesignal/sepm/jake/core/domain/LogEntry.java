a2ffdc2e3e604a1768b304a3f00d960bac7b43f9
johannes <johannes@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-05-26 18:08:30 +0000

    added hash and userid to LogEntry
    git-svn-id: svn://svn.jakeapp.com/trunk@343 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/doublesignal/sepm/jake/core/domain/LogEntry.java
+++ core/src/main/java/com/doublesignal/sepm/jake/core/domain/LogEntry.java
@@ -12,9 +12,11 @@ public class LogEntry {
 	
 	private LogAction action;
 	private Date timestamp;
-	private String comment;
 	private String jakeObjectName;
-
+	private String hash;
+	private String userId;
+	private String comment;
+	
 	/**
 	 * Construct a new LogEntry with the given params.
 	 * @param action The <code>LogAction</code> to be logged
@@ -23,11 +25,14 @@ public class LogEntry {
 	 * entry corresponds to.
 	 * @param comment An arbitrary comment
 	 */
-	public LogEntry(LogAction action, Date timestamp, String jakeObjectName, String comment) {
+	public LogEntry(LogAction action, Date timestamp, String jakeObjectName, 
+			String hash, String userId, String comment) {
 		this.action = action;
 		this.timestamp = timestamp;
 		this.comment = comment;
 		this.jakeObjectName = jakeObjectName;
+		this.hash = hash;
+		this.userId = userId;
 	}
 
 	public LogAction getAction() {
@@ -62,34 +67,20 @@ public class LogEntry {
 		this.jakeObjectName = jakeObjectName;
 	}
 
-	/**
-	 * Test if two LogEntrys are equal.
-	 * @return <code>true</code> iff all fields are equal.
-	 */
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
+	public String getHash() {
+		return hash;
+	}
 
-		LogEntry logEntry = (LogEntry) o;
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
 
-		if (action != logEntry.action) {
-			return false;
-		}
-		if (comment != null ? !comment.equals(logEntry.comment) : logEntry.comment != null) {
-			return false;
-		}
-		if (jakeObjectName != null ? !jakeObjectName.equals(logEntry.jakeObjectName) : logEntry.jakeObjectName != null) {
-			return false;
-		}
-		if (timestamp != null ? !timestamp.equals(logEntry.timestamp) : logEntry.timestamp != null) {
-			return false;
-		}
+	public String getUserId() {
+		return userId;
+	}
 
-		return true;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 
 	/**
@@ -103,5 +94,35 @@ public class LogEntry {
 		result = 28 * result + (comment != null ? comment.hashCode() : 0);
 		result = 28 * result + (jakeObjectName != null ? jakeObjectName.hashCode() : 0);
 		return result;
+	}
+
+	/**
+	 * Test if two LogEntrys are equal.
+	 * @return <code>true</code> iff all fields are equal.
+	 */
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+	
+		LogEntry logEntry = (LogEntry) o;
+	
+		if (action != logEntry.action) {
+			return false;
+		}
+		if (comment != null ? !comment.equals(logEntry.comment) : logEntry.comment != null) {
+			return false;
+		}
+		if (jakeObjectName != null ? !jakeObjectName.equals(logEntry.jakeObjectName) : logEntry.jakeObjectName != null) {
+			return false;
+		}
+		if (timestamp != null ? !timestamp.equals(logEntry.timestamp) : logEntry.timestamp != null) {
+			return false;
+		}
+	
+		return true;
 	}
 }
