cb6f42dc607b49de38c420421db4ef4683d21c66
johannes <johannes@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-05-26 22:35:52 +0000

    added comment + suppressed serial
    git-svn-id: svn://svn.jakeapp.com/trunk@346 cfed719c-3465-4289-a5de-482d64f03f8e

--- gui/src/main/java/com/doublesignal/sepm/jake/gui/i18n/exceptions/IllegalNumberOfArgumentsException.java
+++ gui/src/main/java/com/doublesignal/sepm/jake/gui/i18n/exceptions/IllegalNumberOfArgumentsException.java
@@ -1,8 +1,9 @@
 package com.doublesignal.sepm.jake.gui.i18n.exceptions;
 
 /**
- *
+ * The number of arguments to fill in was not correct.
  */
+@SuppressWarnings("serial")
 public class IllegalNumberOfArgumentsException extends Exception {
 	public IllegalNumberOfArgumentsException(String message) {
 		super(message);	
