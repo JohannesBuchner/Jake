606081404d4777a2f1a9fc6c913bcb451e53196a
Chris <Chris@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-05-31 14:30:33 +0000

    Updated DB setup script and DB description to match deleted type (can't take care of ER diagram right now because I don't have Dia on this computer, will do that tomorrow)
    git-svn-id: svn://svn.jakeapp.com/trunk@375 cfed719c-3465-4289-a5de-482d64f03f8e

--- documentation/db/db-setup.sql
+++ documentation/db/db-setup.sql
@@ -1,46 +1,49 @@
-CREATE TABLE `configuration` (
-	name				VARCHAR(50)		PRIMARY KEY,
-	value				VARCHAR(255)
-);
+  DROP TABLE IF EXISTS configuration CASCADE;
+  DROP TABLE IF EXISTS projectmembers CASCADE;
+  DROP TABLE IF EXISTS objects CASCADE;
+  DROP TABLE IF EXISTS noteobjects CASCADE;
+  DROP TABLE IF EXISTS tags CASCADE;
+  DROP TABLE IF EXISTS logentries CASCADE;
 
-CREATE TABLE `projectmembers` (
-	userid			VARCHAR(255)	PRIMARY KEY,
-	nick				VARCHAR(50),
-	notes				LONGVARCHAR
-);
+  CREATE TABLE configuration (
+  	name				VARCHAR(50)		PRIMARY KEY,
+  	value				VARCHAR(255)
+  );
 
-CREATE TABLE `objects` (
-	name				VARCHAR,
-	type				TINYINT,
-	PRIMARY KEY (name, type)
-);
+  CREATE TABLE projectmembers (
+  	userid			VARCHAR(255)	PRIMARY KEY,
+  	nick				VARCHAR(50),
+  	notes				LONGVARCHAR
+  );
 
-CREATE TABLE `noteobjects` (
-	name				VARCHAR,
-	content			VARCHAR NOT NULL,
-	PRIMARY KEY (name),
-	FOREIGN KEY (name) REFERENCES objects(name)
-);
+  CREATE TABLE objects (
+  	name				VARCHAR,
+  	PRIMARY KEY (name)
+  );
 
-CREATE TABLE `tags` (
-	object_name		VARCHAR,
-	object_type		TINYINT,
-	tag				VARCHAR(50),
-	PRIMARY KEY(object_name, object_type, tag),	
-	FOREIGN KEY (object_name, object_type) REFERENCES objects(name, type)
-);
+  CREATE TABLE noteobjects (
+  	name				VARCHAR,
+  	content			VARCHAR NOT NULL,
+  	PRIMARY KEY (name),
+  	FOREIGN KEY (name) REFERENCES objects(name)
+  );
 
-CREATE TABLE `logentries` (
-	object_name		VARCHAR,
-	object_type		TINYINT,
-	projectmember	VARCHAR(255),
-	timestamp		DATETIME,
-	action			VARCHAR(10) NOT NULL,
-	message			VARCHAR(255),
-	hash				VARCHAR(65) NOT NULL,
-	is_last_pulled	BOOLEAN,
-	PRIMARY KEY(object_name, object_type, projectmember, timestamp),	
-	FOREIGN KEY (object_name, object_type) REFERENCES objects(name, type),
-	FOREIGN KEY (projectmember) REFERENCES projectmembers(userid)
-);
+  CREATE TABLE tags (
+  	object_name		VARCHAR,
+  	tag				VARCHAR(50),
+  	PRIMARY KEY(object_name, tag),
+  	FOREIGN KEY (object_name) REFERENCES objects(name)
+  );
 
+  CREATE TABLE logentries (
+  	object_name		VARCHAR,
+  	projectmember	VARCHAR(255),
+  	timestamp		DATETIME,
+  	action			VARCHAR(10) NOT NULL,
+  	message			VARCHAR(255),
+  	hash				VARCHAR(65) NOT NULL,
+  	is_last_pulled	BOOLEAN,
+  	PRIMARY KEY(object_name, projectmember, timestamp),
+  	FOREIGN KEY (object_name) REFERENCES objects(name),
+  	FOREIGN KEY (projectmember) REFERENCES projectmembers(userid)
+  );
