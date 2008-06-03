3b973074112f174f6df7c3948aa94042b975c19c
peter <peter@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-06-03 14:04:33 +0000

    initial up johannes says "SORRY" ;)
    git-svn-id: svn://svn.jakeapp.com/trunk@419 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ core/src/main/java/com/doublesignal/sepm/jake/core/services/exceptions/NoProjectLoadedException.java
@@ -0,0 +1,11 @@
+package com.doublesignal.sepm.jake.core.services.exceptions;
+
+/**
+ * Project is not loaded.
+ * 
+ * @author studpete
+ * 
+ */
+public class NoProjectLoadedException extends GuiCoreInteractionException {
+
+}
