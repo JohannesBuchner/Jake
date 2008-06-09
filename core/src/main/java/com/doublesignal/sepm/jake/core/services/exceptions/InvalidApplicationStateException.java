812a1cc637a6e8e80d370b8093c8389e0ff5ad51
johannes <johannes@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-06-09 00:02:35 +0000

    This runtime exception should replace all "Cannot be reached", "Won't happen" comments/logs
    git-svn-id: svn://svn.jakeapp.com/trunk@526 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ core/src/main/java/com/doublesignal/sepm/jake/core/services/exceptions/InvalidApplicationStateException.java
@@ -0,0 +1,11 @@
+package com.doublesignal.sepm.jake.core.services.exceptions;
+
+/**
+ * This should replace all "This can not happen" log-entries or comments
+ * @author johannes
+ *
+ */
+@SuppressWarnings("serial")
+public class InvalidApplicationStateException extends RuntimeException {
+
+}
