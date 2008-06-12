eb7627b4bb92a3f1d2ac76057c81b533174b4373
philipp <philipp@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-06-12 10:41:30 +0000

    test for getLog
    git-svn-id: svn://svn.jakeapp.com/trunk@594 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/test/java/com/doublesignal/sepm/jake/core/services/JakeGuiAccessTest.java
+++ core/src/test/java/com/doublesignal/sepm/jake/core/services/JakeGuiAccessTest.java
@@ -1,17 +1,29 @@
 package com.doublesignal.sepm.jake.core.services;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
 import java.util.Date;
+import java.util.Random;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import junit.framework.TestCase;
 
+import com.doublesignal.sepm.jake.core.dao.HsqlJakeDatabase;
+import com.doublesignal.sepm.jake.core.dao.JdbcConfigurationDao;
+import com.doublesignal.sepm.jake.core.dao.JdbcJakeObjectDao;
+import com.doublesignal.sepm.jake.core.dao.JdbcLogEntryDao;
+import com.doublesignal.sepm.jake.core.dao.JdbcProjectMemberDao;
 import com.doublesignal.sepm.jake.core.dao.exceptions.NoSuchProjectMemberException;
 import com.doublesignal.sepm.jake.core.domain.JakeObject;
+import com.doublesignal.sepm.jake.core.domain.NoteObject;
 import com.doublesignal.sepm.jake.core.domain.LogAction;
 import com.doublesignal.sepm.jake.core.domain.LogEntry;
 import com.doublesignal.sepm.jake.core.domain.ProjectMember;
@@ -28,7 +40,18 @@ public class JakeGuiAccessTest extends TestCase {
 	IJakeGuiAccess jga;
 	ProjectMember pm;
 	Date date;
+	private static JdbcConfigurationDao dao;
 	
+	
+	@BeforeClass
+	public static void setDAO() {
+		dao = new JdbcConfigurationDao();
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("org.hsqldb.jdbcDriver");
+		ds.setUrl("jdbc:hsqldb:mem:dbNewNoSchema");
+		ds.setUsername("sa");
+		dao.setDataSource(ds);
+	}
 	
 	@Before
 	public void setup() throws Exception{
@@ -131,63 +154,91 @@ public class JakeGuiAccessTest extends TestCase {
 	public void testGetLog()throws Exception	{
 		setup();
 		String userId = "testuser@domain.de";
-		String note = "This is a Note!";
 		String hash = "4567897";
 		String comment = "This is a comment!";
-		String noteObjectName = null;
-		
-		jga.login(userId, userId);
-		jga.addProjectMember(userId);
-		jga.createNote(note);
-		date = new Date();
-		noteObjectName = jga.getNotes().get(jga.getNotes().size()-1).getName();
-		
-		LogEntry logEntry = new LogEntry(LogAction.TAG_ADD, date, noteObjectName, hash, userId, comment);
-		jga.createLog(logEntry);
-		
-		assertTrue(jga.getLog().get(jga.getLog().size()-1).equals(logEntry));
-		
-		
-	}
-	
-	@Test
-	public void testGetJakeObjectLog()throws Exception	{
-		setup();
-		
-		String userId = "testuser@domain.de";
-		String note = "This is a Note!";
-		String hash = "4567897";
-		String comment = "This is a comment!";
-		String jakeObjectName = "jakeObject";
-		
-		String userId2 = "user@domain.de";
-		String note2 = "This is a second is a Note!";
-		String hash2 = "23745953";
-		String comment2 = "This is also a comment!";
-		String noteObjectName2 = null;
-		String jakeObjectName2 = "jakeObject2";
-		
-		jga.login(userId, userId);
-		jga.addProjectMember(userId);
-		jga.createNote(note);
-		jga.createNote(note2);
+		String jakeObjectName = "'note::1213053294203";
+		String jakeObjectName2 = "'note::1213953294203";
 		date = new Date();
 		
+		HsqlJakeDatabase db = setUpDatabase();
 		
 		JakeObject jakeObject = new JakeObject(jakeObjectName);
+		NoteObject noteObject = new NoteObject(jakeObjectName, jakeObjectName);
+		ProjectMember projectMember = new ProjectMember(userId);
+		
 		JakeObject jakeObject2 = new JakeObject(jakeObjectName2);
+		NoteObject noteObject2 = new NoteObject(jakeObjectName2, jakeObjectName2);
 		
 		
-		LogEntry logEntry = new LogEntry(LogAction.TAG_ADD, date, jakeObjectName, hash, userId, comment);
-		LogEntry logEntry2 = new LogEntry(LogAction.TAG_ADD, date, jakeObjectName2, hash, userId, comment);
+		db.getProjectMemberDao().save(projectMember);
+		db.getJakeObjectDao().save(jakeObject);
+		db.getJakeObjectDao().save(noteObject);
+		db.getJakeObjectDao().save(jakeObject2);
+		db.getJakeObjectDao().save(noteObject2);
+		
+		LogEntry logEntry = new LogEntry(LogAction.TAG_ADD, date,jakeObjectName, hash, userId, comment);
+		LogEntry logEntry3 = new LogEntry(LogAction.NEW_VERSION, date,jakeObjectName2, hash, userId, comment);
+		
+		db.getLogEntryDao().create(logEntry);
+		db.getLogEntryDao().create(logEntry3);
+		
+		assertTrue(db.getLogEntryDao().getAll().size()==2);
+		assertTrue(db.getLogEntryDao().getAll().get(0).equals(logEntry));
+		assertFalse(db.getLogEntryDao().getAll().get(0).equals(db.getLogEntryDao().getAll().get(1)));
+	}
 
+	
+	public static HsqlJakeDatabase setUpDatabase() throws Exception {
+		return setUpDatabase(null);
+	}
+
+	public static HsqlJakeDatabase setUpDatabase(String dbfile) throws Exception {
+		HsqlJakeDatabase db = new HsqlJakeDatabase();
+		JdbcConfigurationDao cd = new JdbcConfigurationDao();
+		JdbcJakeObjectDao jod = new JdbcJakeObjectDao();
+		JdbcProjectMemberDao pmd = new JdbcProjectMemberDao();
+		JdbcLogEntryDao led = new JdbcLogEntryDao();
 		
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("org.hsqldb.jdbcDriver");
+		Random r = new Random();
+		if(dbfile == null)
+			ds.setUrl("jdbc:hsqldb:mem:newAndEmptyDatabase" + r.nextInt()+";ifexists=false");
+		else
+			ds.setUrl("jdbc:hsqldb:file:" + dbfile + ";ifexists=false");
+		ds.setUsername("sa");
 		
+		cd.setDataSource(ds);
+		jod.setDataSource(ds);
+		pmd.setDataSource(ds);
+		led.setDataSource(ds);
 		
+		ClassPathResource scriptres = new ClassPathResource("skeleton.script");
+		BufferedReader fis = new BufferedReader(new FileReader(scriptres.getFile()));
 		
-		
-		
-		
+		while (true) {
+			String l = fis.readLine();
+			if (l == null)
+				break;
+			if (l.startsWith("CREATE TABLE ") || l.startsWith("INSERT ") || 
+					l.startsWith("UPDATE ") || l.startsWith("DELETE ")) {
+				ds.getConnection().createStatement().execute(l);
+			}
+		}
+		if (fis != null)
+			fis.close();
+		ds.getConnection().commit();
+		db.setConfigurationDao(cd);
+		db.setJakeObjectDao(jod);
+		db.setProjectMemberDao(pmd);
+		db.setLogEntryDao(led);
+		return db;
+	}
+	public static void teardownDatabase(HsqlJakeDatabase db) throws Exception{
+		Connection c = ((JdbcConfigurationDao)db.getConfigurationDao()).getDataSource().
+			getConnection();
+		c.rollback();
+		c.close();
 	}
 	
 	@After
