ebb9810cea71047f255ea83512f4dc93c09a0b1a
johannes <johannes@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-06-18 14:11:39 +0000

    not tested nor compiled - Overriding language at runtime
    git-svn-id: svn://svn.jakeapp.com/trunk@668 cfed719c-3465-4289-a5de-482d64f03f8e

--- gui/src/main/java/com/doublesignal/sepm/jake/gui/StartJake.java
+++ gui/src/main/java/com/doublesignal/sepm/jake/gui/StartJake.java
@@ -1,5 +1,7 @@
 package com.doublesignal.sepm.jake.gui;
 
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
 
 import com.doublesignal.sepm.jake.gui.i18n.ITranslationProvider;
@@ -18,12 +20,21 @@ public class StartJake
 	{
 		log.info(translator.get("ConsoleWelcome"));
 		log.debug("starting frontend....");
+		if(args[0].startsWith("--lang=")){
+			String file = args[0].substring("--lang=".length());
+			try{
+				translator.setLanguage(file);
+			}catch(IOException e){
+				log.fatal("language file " + file + " not found");
+				return -1;
+			}
+		}
 		JakeGui.setSystemProperties();
 		JakeGui.setNativeLookAndFeel();
 		if(args.length==0){
 			JakeGui.showSelectProjectDialog(null);
 		}else{
-			JakeGui.showSelectProjectDialog(args[0]);
+			JakeGui.showSelectProjectDialog(args[args.length - 1]);
 		}
 		
 		log.debug("loading done.");
