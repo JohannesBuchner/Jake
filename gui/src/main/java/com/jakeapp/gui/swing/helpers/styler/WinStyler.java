d7ea8445fbf6033182b3839d77405ef25c3b57c4
peter <peter@cfed719c-3465-4289-a5de-482d64f03f8e>
2008-12-30 10:41:17 +0000

    added style for windows: make toggled buttons font bold. (the toggle effect was bearly visible with win xp default theme...)
    git-svn-id: svn://svn.jakeapp.com/trunk@891 cfed719c-3465-4289-a5de-482d64f03f8e

--- gui/src/main/java/com/jakeapp/gui/swing/helpers/styler/WinStyler.java
+++ gui/src/main/java/com/jakeapp/gui/swing/helpers/styler/WinStyler.java
@@ -5,9 +5,20 @@
 
 package com.jakeapp.gui.swing.helpers.styler;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * @author studpete
  */
 public class WinStyler extends AbstractStyler {
 
+    @Override
+    public void styleToolbarButton(JToggleButton btn) {
+        if (btn.isSelected()) {
+            btn.setFont(btn.getFont().deriveFont(Font.BOLD));
+        } else {
+            btn.setFont(btn.getFont().deriveFont(Font.PLAIN));
+        }
+    }
 }
