21b7f7b8fb13869e730a5b462a036fd42a4407b6
chris <chris@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-06 11:03:36 +0000

    Fixed double method definition - Jake wouldn't compile. Böser Dominik! ;-)
    git-svn-id: svn://svn.jakeapp.com/trunk@947 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/domain/ProjectMember.java
+++ core/src/main/java/com/jakeapp/core/domain/ProjectMember.java
@@ -15,104 +15,87 @@ import javax.persistence.Column;
  */
 @Entity
 public class ProjectMember implements ILogable {
-    private static final long serialVersionUID = -9208035210417004558L;
-    private UserId userId;
-    private TrustState trustState;
-    private String nickname;
-    
-    /**
-     * Default constructor.
-     */
-    public ProjectMember() {
-            this.trustState = TrustState.NO_TRUST;
-    }
+   private static final long serialVersionUID = -9208035210417004558L;
+   private UserId userId;
+   private TrustState trustState;
+   private String nickname;
 
-    /**
-     * Construct a new <code>PrjectMember</code>.
-     *
-     * @param userId     the userId
-     * @param trustState the trust state of the <code>ProjectMember</code>
-     */
-    public ProjectMember(UserId userId, TrustState trustState) {
-        this.userId = userId;
-        this.setTrustState(trustState);
-    }
-
-    /**
-     * Set the <code>trustState</code> of this <code>ProjectMember</code>.
-     *
-     * @param trustState the level of trust we have to the <code>ProjectMember</code>.
-     * @see TrustState
-     */
-    public void setTrustState(TrustState trustState) {
-        this.trustState = trustState;
-    }
-
-	/**
-	 * @return The level of trust the user has to this projectMember.
-	 */
-	@Column(name="trustlevel")
-    public TrustState getTrustState() {
-		return this.trustState;
-	}
-    
-
-    /**
-     * @return the <code>userId</code> of this <code>ProjectMember</code>.
+   /**
+    * Default constructor.
     */
-    @Id
-    @Column(name = "memberId")
-    public UserId getUserId() {
-        return this.userId;
-    }
+   public ProjectMember() {
+      this.trustState = TrustState.NO_TRUST;
+   }
+
+   /**
+    * Construct a new <code>PrjectMember</code>.
+    *
+    * @param userId     the userId
+    * @param trustState the trust state of the <code>ProjectMember</code>
+    */
+   public ProjectMember(UserId userId, TrustState trustState) {
+      this.userId = userId;
+      this.setTrustState(trustState);
+   }
+
+   /**
+    * Set the <code>trustState</code> of this <code>ProjectMember</code>.
+    *
+    * @param trustState the level of trust we have to the <code>ProjectMember</code>.
+    *
+    * @see TrustState
+    */
+   public void setTrustState(TrustState trustState) {
+      this.trustState = trustState;
+   }
+
+   /**
+    * @return The level of trust the user has to this projectMember.
+    */
+   @Column(name = "trustlevel")
+   public TrustState getTrustState() {
+      return this.trustState;
+   }
 
 
-    /**
-     * @return The level of trust the user has to this projectMember.
-     */
-    public TrustState getTrustState() {
-        return this.trustState;
-    }
+   /**
+    * @return the <code>userId</code> of this <code>ProjectMember</code>.
+    */
+   @Id
+   @Column(name = "memberId")
+   public UserId getUserId() {
+      return this.userId;
+   }
 
-    /**
-     * @return the <code>userId</code> of this <code>ProjectMember</code>.
-     */
-    @Id
-    public UserId getUserId() {
-        return this.userId;
-    }
+   public void setUserId(UserId userId) {
+      this.userId = userId;
+   }
 
-    public void setUserId(UserId userId) {
-        this.userId = userId;
-    }
+   /**
+    * The String representation of the ProjectMember
+    *
+    * @return string of ProjectMember
+    */
+   public String toString() {
+      return getUserId() + " trust: " + getTrustState();
+   }
 
-    /**
-     * The String representation of the ProjectMember
-     *
-     * @return string of ProjectMember
-     */
-    public String toString() {
-        return getUserId() + " trust: " + getTrustState();
-    }
+   @Transient
+   private String getUserIdString() {
+      return this.userId.getUuid().toString();
+   }
 
-    @Transient
-    private String getUserIdString()
-    {
-        return this.userId.getUuid().toString();
-    }
-
-    private void setUserIdString(String uuid)
-    {
-        //this.userId = new // TODO 
-    }
+   private void setUserIdString(String uuid) {
+      //this.userId = new // TODO
+   }
 
 
-    @Column(name="nickname", length = 100)
-    public String getNickname() {
-        return nickname;
-    }
+   @Column(name = "nickname", length = 100)
+   public String getNickname() {
+      return nickname;
+   }
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
+   public void setNickname(String nickname) {
+      this.nickname = nickname;
+   }
 }
