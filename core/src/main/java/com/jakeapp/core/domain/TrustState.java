a860b76186103a241f5c9c49bd1dc8fe9d0a4121
peter <peter@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-06 17:58:37 +0000

    removed toString for enum. this works automatically, explicit toString caused stack overflow!
    git-svn-id: svn://svn.jakeapp.com/trunk@966 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/domain/TrustState.java
+++ core/src/main/java/com/jakeapp/core/domain/TrustState.java
@@ -11,48 +11,34 @@ package com.jakeapp.core.domain;
  * Version: ${MAVEN-VERSION}
  */
 public enum TrustState {
-    /**
-     * AUTO_ADD_REMOVE: (formerly FULL_TRUST)
-     * Means, "I" trust this ProjectMember and follow his/her list of trusted users. 
-     * If he/she adds or removes other members to his/her list, I do so to.
-     */
-    AUTO_ADD_REMOVE(2),
+	/**
+	 * AUTO_ADD_REMOVE: (formerly FULL_TRUST)
+	 * Means, "I" trust this ProjectMember and follow his/her list of trusted users.
+	 * If he/she adds or removes other members to his/her list, I do so to.
+	 */
+	AUTO_ADD_REMOVE(2),
 
-    /**
-     * TRUST: I "normally" trust this ProjectMember, files will be
-     * pulled from him or her.
-     */
-    TRUST(1),
-
-
-    /**
-     * NO_TRUST: I either don't know this ProjectMember (person not in Project)
-     * or explicitly don't trust him.
-     * The other user is just another project member, no files will be
-     * pulled from this member. The other member does not belong
-     * to this Nodes neighborhood.
-     * This is the default level of trust.
-     */
-    NO_TRUST(0);
+	/**
+	 * TRUST: I "normally" trust this ProjectMember, files will be
+	 * pulled from him or her.
+	 */
+	TRUST(1),
 
 
-    private int trustState;
-
-    private TrustState(int state) {
-        this.trustState = state;
-    }
-
-    public String toString() {
-        switch (trustState) {
-            default:
-            case 0:
-                return TrustState.NO_TRUST.toString();
-            case 1:
-                return TrustState.TRUST.toString();
-            case 2:
-                return TrustState.AUTO_ADD_REMOVE.toString();
-        }
-    }
+	/**
+	 * NO_TRUST: I either don't know this ProjectMember (person not in Project)
+	 * or explicitly don't trust him.
+	 * The other user is just another project member, no files will be
+	 * pulled from this member. The other member does not belong
+	 * to this Nodes neighborhood.
+	 * This is the default level of trust.
+	 */
+	NO_TRUST(0);
 
 
+	private int trustState;
+
+	private TrustState(int state) {
+		this.trustState = state;
+	}
 }
