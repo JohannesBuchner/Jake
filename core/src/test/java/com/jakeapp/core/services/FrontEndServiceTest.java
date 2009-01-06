5e3563de034a593a65a53415e05936d433411bd5
christopher <christopher@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-06 13:27:52 +0000

    Service-Testcases
    git-svn-id: svn://svn.jakeapp.com/trunk@952 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ core/src/test/java/com/jakeapp/core/services/FrontEndServiceTest.java
@@ -0,0 +1,87 @@
+package com.jakeapp.core.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.jakeapp.TestingConstants;
+import com.jakeapp.core.domain.exceptions.InvalidCredentialsException;
+import com.jakeapp.core.domain.exceptions.NotLoggedInException;
+
+
+public class FrontEndServiceTest {
+	private static final String SESSIONID1 = "paula";
+	private static final String SESSIONID2 = "brillant";
+	
+	private static Map<String,String> VALID_CREDENTIALS;
+	
+	private IFrontendService service;
+
+	/**
+	 * @return the service
+	 */
+	private IFrontendService getService() {
+		return service;
+	}
+
+	/**
+	 * @param service the service to set
+	 */
+	private void setService(IFrontendService service) {
+		this.service = service;
+	}
+
+	@BeforeClass
+	public void setUpClass() {
+		VALID_CREDENTIALS = new HashMap<String,String>();
+		//TODO find out valid credentials
+		VALID_CREDENTIALS.put("to", "do");
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		this.setService(new FrontendServiceImpl());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test(timeout = TestingConstants.UNITTESTTIME,expected = InvalidCredentialsException.class)
+	public void authenticate_shouldFailWithEmptyCredentials() throws IllegalArgumentException, InvalidCredentialsException {
+		this.getService().authenticate(new HashMap<String,String>());
+	}
+
+	@Test(timeout = TestingConstants.UNITTESTTIME, expected = NotLoggedInException.class)
+	public void logout_shouldFailForIllegalSession() throws IllegalArgumentException, NotLoggedInException {
+		this.getService().logout(SESSIONID2);
+	}
+	
+	@Test(timeout = TestingConstants.UNITTESTTIME, expected = NotLoggedInException.class)
+	public void logout_shouldFailSecondTime() throws IllegalArgumentException, NotLoggedInException, InvalidCredentialsException {
+		String sessionid = this.getService().authenticate(VALID_CREDENTIALS);
+		this.getService().logout(sessionid);
+		
+		//CALL
+		this.getService().logout(sessionid);
+		//CALL
+	}
+	
+	@Test(timeout = TestingConstants.UNITTESTTIME)
+	public void logout_shouldSucceed() throws IllegalArgumentException, NotLoggedInException, InvalidCredentialsException {
+		String sessionid = this.getService().authenticate(VALID_CREDENTIALS);
+		
+		//CALL
+		this.getService().logout(sessionid);
+		//CALL
+	}
+
+	@Test(timeout = TestingConstants.UNITTESTTIME,expected = NotLoggedInException.class)
+	public void ping_shouldFailWithNoLogin() throws IllegalArgumentException, NotLoggedInException {
+		this.getService().ping(SESSIONID1);
+	}
+}
