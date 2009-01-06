53ad522db80ed63023d8b3377d83103ce873f44c
christopher <christopher@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-06 17:24:12 +0000

    Added some methods to manage a FrontendService's sessions.
    git-svn-id: svn://svn.jakeapp.com/trunk@964 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/services/FrontendServiceImpl.java
+++ core/src/main/java/com/jakeapp/core/services/FrontendServiceImpl.java
@@ -1,12 +1,13 @@
 package com.jakeapp.core.services;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import com.jakeapp.core.domain.exceptions.InvalidCredentialsException;
 import com.jakeapp.core.domain.exceptions.NotLoggedInException;
 import com.jakeapp.jake.ics.ICService;
-import com.jakeapp.jake.ics.msgservice.IMsgService;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,24 +17,104 @@ import com.jakeapp.jake.ics.msgservice.IMsgService;
  * To change this template use File | Settings | File Templates.
  */
 public class FrontendServiceImpl implements IFrontendService {
+	
+	private Map<String,FrontendSession> sessions;
+	
+	/**
+	 * Checks frontend-credentials and throws exceptions if they
+	 * are not correct.
+	 * @see #authenticate(Map)
+	 * @param credentials The credentials to be checked
+	 */
+	private void checkCredentials(Map<String, String> credentials)
+		throws IllegalArgumentException, InvalidCredentialsException { 
+		
+		if (credentials == null) throw new IllegalArgumentException();
+		
+		//TODO do further checking for later versions
+	}
+	
+	private void setSessions(Map<String,FrontendSession> sessions) {
+		this.sessions = sessions;
+	}
+
+	private Map<String,FrontendSession> getSessions() {
+		return sessions;
+	}
+	
+	private void addSession(String sessid,FrontendSession session) {
+		this.getSessions().put(sessid,session);
+	}
+	
+	private boolean removeSession(String sessid) {
+		FrontendSession fes;
+		
+		fes = this.getSessions().remove(sessid);
+		
+		return fes != null;
+	}
+	
+	/**
+	 * retrieves a session
+	 * @param sessionId The id associated with the session after it was created
+	 * @throws IllegalArgumentException if <code>sessionId</code> was null
+	 * @throws NotLoggedInException if no Session associated with <code>sessionId</code> exists.
+	 */
+	private FrontendSession getSession(String sessionId) throws IllegalArgumentException, NotLoggedInException {
+		FrontendSession result;
+		
+		if (sessionId == null) throw new IllegalArgumentException();
+		result = this.getSessions().get(sessionId);
+		
+		if (result == null) throw new NotLoggedInException();
+		
+		return result;
+	}
+	
+    /**
+     * @return A sessionid for a new session
+     */
+	private String makeSessionID() {
+		return UUID.randomUUID().toString();
+	}
+	
+	public FrontendServiceImpl() {
+		this.setSessions(new HashMap<String,FrontendSession>());
+	}
+	
     @Override
     public String authenticate(Map<String, String> credentials) throws IllegalArgumentException, InvalidCredentialsException {
-        return null; // TODO
+    	String sessid;
+    	
+    	this.checkCredentials(credentials);
+    	
+    	//create new session
+    	sessid = makeSessionID();
+    	this.addSession(sessid, new FrontendSession());
+    	
+        return sessid;
     }
 
     @Override
     public boolean logout(String sessionId) throws IllegalArgumentException, NotLoggedInException {
-        return false; // TODO
+    	boolean successfullyRemoved;
+    	
+    	if (sessionId == null) throw new IllegalArgumentException();
+    	
+    	successfullyRemoved = this.removeSession(sessionId);
+    	if (!successfullyRemoved) throw new NotLoggedInException();
+    	
+        return true;
     }
 
     @Override
     public IMetaProjectService getMetaProjectService(String sessionId) throws IllegalArgumentException, NotLoggedInException, IllegalStateException {
-        return null; // TODO
+        return this.getSession(sessionId).getMetaProjectService();
     }
 
     @Override
     public List<ICService> getICServices(String sessionId) throws IllegalArgumentException, NotLoggedInException, IllegalStateException {
-        return null; // TODO
+    	return this.getSession(sessionId).getICServices();
     }
 
     @Override
---
+++ core/src/main/java/com/jakeapp/core/services/FrontendSession.java
@@ -0,0 +1,25 @@
+package com.jakeapp.core.services;
+
+import java.util.List;
+
+import com.jakeapp.jake.ics.ICService;
+
+/**
+ * Session of work that is established whenever any Frontend
+ * accesses the core. The session contains references to all
+ * Services needed by the frontend.
+ * @author christopher
+ */
+public class FrontendSession {
+	public FrontendSession() {
+		//TODO create and store ics-Service-Implementations
+	}
+	
+    public IMetaProjectService getMetaProjectService() throws  IllegalStateException {
+        return null; // TODO
+    }
+
+    public List<ICService> getICServices() throws  IllegalStateException {
+        return null; // TODO
+    }
+}
