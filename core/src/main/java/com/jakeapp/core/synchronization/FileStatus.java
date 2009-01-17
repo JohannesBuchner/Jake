52e70f3de27cdfbbc3abba233f58e110c718a5e6
chris <chris@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-17 15:05:05 +0000

    File status extended
    git-svn-id: svn://svn.jakeapp.com/trunk@1104 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/synchronization/FileStatus.java
+++ core/src/main/java/com/jakeapp/core/synchronization/FileStatus.java
@@ -2,27 +2,44 @@ package com.jakeapp.core.synchronization;
 
 /**
  * struct for giving over to the gui
- * 
+ *
  * @author johannes
  */
 public class FileStatus {
-
 	private String filename;
-
-	private boolean inConflict;
 
 	private long lastModification;
 
+	/**
+	 * This file has been modified by *us*, but hasn't been announced yet
+	 */
 	private boolean locallyModified;
 
-	
-	public FileStatus(String filename, boolean inConflict, long lastModification,
-			boolean locallyModified) {
+	/**
+	 * This file has been modified by someone else, but hasn't been pulled yet
+	 */
+	private boolean remotelyModified;
+
+	/**
+	 * This file only exists locally.
+	 */
+	private boolean onlyLocal;
+
+	/**
+	 * This file only exists in the cloud.
+	 */
+	private boolean onlyRemote;
+
+	public FileStatus(String filename, long lastModification,
+	                  boolean locallyModified, boolean remotelyModified,
+	                  boolean onlyLocal, boolean onlyRemote) {
 		super();
 		this.filename = filename;
-		this.inConflict = inConflict;
 		this.lastModification = lastModification;
 		this.locallyModified = locallyModified;
+		this.remotelyModified = remotelyModified;
+		this.onlyLocal = onlyLocal;
+		this.onlyRemote = onlyRemote;
 	}
 
 
@@ -30,19 +47,39 @@ public class FileStatus {
 		return this.filename;
 	}
 
-	
+
+	/**
+	 * Whether or not this file is in conflict (i.e. there is a local file and a remote file
+	 * that have both been modified)
+	 */
 	public boolean isInConflict() {
-		return this.inConflict;
+		return this.locallyModified && this.remotelyModified;
 	}
 
-	
+	/**
+	 * Whether or not we have the lastest version of the file
+	 */
+	public boolean isLocalLatest() {
+		return !(this.locallyModified || this.remotelyModified);
+	}
+
+
 	public long getLastModification() {
 		return this.lastModification;
 	}
 
-	
+	/**
+	 * Whether or not the file has been modified by *us*
+	 */
 	public boolean isLocallyModified() {
 		return this.locallyModified;
+	}
+
+	/**
+	 * Whether or not the file has been modified by someone else
+	 */
+	public boolean isRemotelyModified() {
+		return this.remotelyModified;
 	}
 
 }
