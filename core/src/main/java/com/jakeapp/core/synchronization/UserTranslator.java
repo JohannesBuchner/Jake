fcb26275c3ebe8c564bf1644151dca2c6c31269b
dominik <dominik@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-18 12:16:22 +0000

    inserted requested code.
    git-svn-id: svn://svn.jakeapp.com/trunk@1163 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/synchronization/UserTranslator.java
+++ core/src/main/java/com/jakeapp/core/synchronization/UserTranslator.java
@@ -1,34 +1,50 @@
 package com.jakeapp.core.synchronization;
 
 import com.jakeapp.core.dao.exceptions.NoSuchProjectMemberException;
+import com.jakeapp.core.dao.exceptions.NoSuchUserIdException;
+import com.jakeapp.core.dao.IUserIdDao;
 import com.jakeapp.core.domain.Project;
 import com.jakeapp.core.domain.ProjectMember;
 import com.jakeapp.core.domain.UserId;
+import com.jakeapp.core.domain.exceptions.InvalidUserIdException;
 import com.jakeapp.core.services.MsgService;
 import com.jakeapp.core.util.ApplicationContextFactory;
 
 
 public class UserTranslator {
 
-	private ApplicationContextFactory applicationContextFactory;
+    private final ApplicationContextFactory applicationContextFactory;
 
-	public UserTranslator(ApplicationContextFactory applicationContextFactory) {
-		this.applicationContextFactory = applicationContextFactory;
-	}
+    private final IUserIdDao userIdDao;
 
-	/** TODO
-	 * I _NEED_ the xmpp id!
-	 * @param project
-	 * @param member
-	 * @return
-	 */
-	public UserId getUserIdFromProjectMember(Project project, ProjectMember member) {
-		return null;
-	}
 
-	public ProjectMember getProjectMemberFromUserId(Project project, UserId userid)
-			throws NoSuchProjectMemberException {
-		return applicationContextFactory.getProjectMemberDao(project).get(
-				userid.getUuid());
+
+    public UserTranslator(ApplicationContextFactory applicationContextFactory, IUserIdDao userIdDao) {
+        this.applicationContextFactory = applicationContextFactory;
+        this.userIdDao = userIdDao;
+    }
+
+    /**
+     * TODO
+     * I _NEED_ the xmpp id!
+     *
+     * @param project
+     * @param member
+     * @return
+     */
+    public UserId getUserIdFromProjectMember(Project project, ProjectMember member) {
+        try {
+            return this.userIdDao.get(member.getUserId());
+        } catch (InvalidUserIdException e) {
+            return null;
+        } catch (NoSuchUserIdException e) {
+            return null;
+        }
+    }
+
+    public ProjectMember getProjectMemberFromUserId(Project project, UserId userid)
+            throws NoSuchProjectMemberException {
+        return applicationContextFactory.getProjectMemberDao(project).get(
+                userid.getUuid());
 	}
 }
