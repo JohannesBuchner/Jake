332282e2ad8becae8d538e2c6f72234ae0cc6924
peter <peter@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-18 18:42:30 +0000

    is now a runtimeexception
    git-svn-id: svn://svn.jakeapp.com/trunk@1204 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/domain/exceptions/NotLoggedInException.java
+++ core/src/main/java/com/jakeapp/core/domain/exceptions/NotLoggedInException.java
@@ -5,22 +5,22 @@ package com.jakeapp.core.domain.exceptions;
  * is not logged
  * in and therefor nor authorized to do so.
  */
-public class NotLoggedInException extends Exception {
-    private static final long serialVersionUID = 7900725036326667321L;
+public class NotLoggedInException extends RuntimeException {
+	private static final long serialVersionUID = 7900725036326667321L;
 
-    public NotLoggedInException() {
-        super();
-    }
+	public NotLoggedInException() {
+		super();
+	}
 
-    public NotLoggedInException(final String message) {
-        super(message);
-    }
+	public NotLoggedInException(final String message) {
+		super(message);
+	}
 
-    public NotLoggedInException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
+	public NotLoggedInException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
 
-    public NotLoggedInException(final Throwable cause) {
-        super(cause);
-    }
+	public NotLoggedInException(final Throwable cause) {
+		super(cause);
+	}
 }
