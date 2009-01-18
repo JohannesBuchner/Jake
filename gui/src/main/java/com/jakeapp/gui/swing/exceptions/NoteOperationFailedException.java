fa68c9a3d4c35e98bf93aae846e7c9b5ff0df02a
simon <simon@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-18 23:36:16 +0000

    d'oh
    git-svn-id: svn://svn.jakeapp.com/trunk@1252 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ gui/src/main/java/com/jakeapp/gui/swing/exceptions/NoteOperationFailedException.java
@@ -0,0 +1,39 @@
+package com.jakeapp.gui.swing.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This Exception is raised whenever a note could not be created.
+ * @author Simon
+ *
+ */
+public class NoteOperationFailedException extends Exception {
+	
+	private List<Exception> nestedExceptions;
+	{
+		this.nestedExceptions = new ArrayList<Exception>();
+	}
+	
+	public void append(Exception e) {
+		this.nestedExceptions.add(e);
+	}
+
+	/**
+	 * Get a list of nested <code>Exception</code>s.
+	 * @return list of nested <code>Exception</code>s
+	 */
+	protected List<Exception> getNestedExceptions() {
+		return this.nestedExceptions;
+	}
+
+	@Override
+	public String getMessage() {
+		String str = new String();
+		str = "CouldNotCreateNoteException: " + this.nestedExceptions.size() + " appended Exceptions. Error message of this Exception: ";
+		str += super.getMessage();
+		return str;
+	}
+	
+	
+}
