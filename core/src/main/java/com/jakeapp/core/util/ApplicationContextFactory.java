87160e370ecb5900a488326e99f6ab3a6d88b2b1
johannes <johannes@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-18 00:19:31 +0000

    added syntactical sugar for dao
    git-svn-id: svn://svn.jakeapp.com/trunk@1135 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/util/ApplicationContextFactory.java
+++ core/src/main/java/com/jakeapp/core/util/ApplicationContextFactory.java
@@ -9,35 +9,43 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.jakeapp.core.dao.IFileObjectDao;
+import com.jakeapp.core.dao.ILogEntryDao;
+import com.jakeapp.core.dao.INoteObjectDao;
+import com.jakeapp.core.dao.IProjectMemberDao;
 import com.jakeapp.core.domain.Project;
 
 /**
- * A factory that creates and configures spring application contexts. 
- * Application contexts for a certain <code>Project</code>are only 
- * created once and then reused. 
+ * A factory that creates and configures spring application contexts.
+ * Application contexts for a certain <code>Project</code>are only created once
+ * and then reused.
+ * 
  * @author Simon
  */
 public class ApplicationContextFactory {
+
 	private static Logger log = Logger.getLogger(ApplicationContextFactory.class);
+
 	private Hashtable<String, ConfigurableApplicationContext> contextTable;
+
 	private String[] configLocation;
 
 
-    public ApplicationContextFactory()
-	{
-        log.debug("Creating the ApplicationContextFactory");
+	public ApplicationContextFactory() {
+		log.debug("Creating the ApplicationContextFactory");
 
-		this.contextTable = new Hashtable<String, ConfigurableApplicationContext>(); 
+		this.contextTable = new Hashtable<String, ConfigurableApplicationContext>();
 	}
-	
+
 	/**
 	 * Get the <code>ApplicationContext</code> for a given <code>
-	 * Project</code>. If an <code>ApplicationContext</code> for the 
-	 * given <code>Project</code> already exists, the existing context
-	 * is returned, otherwise a new context will be created. This 
-	 * method is <code>synchronized</code>
-	 * @param project the project for which the application context is
-	 * used 
+	 * Project</code>. If an
+	 * <code>ApplicationContext</code> for the given <code>Project</code>
+	 * already exists, the existing context is returned, otherwise a new context
+	 * will be created. This method is <code>synchronized</code>
+	 * 
+	 * @param project
+	 *            the project for which the application context is used
 	 * @return the <code>ApplicationContext</code>
 	 */
 	public synchronized ApplicationContext getApplicationContext(Project project) {
@@ -46,7 +54,8 @@ public class ApplicationContextFactory {
 
 		log.debug("acquiring context for project: " + project.getProjectId());
 		if (this.contextTable.containsKey(project.getProjectId())) {
-			log.debug("context for project: " + project.getProjectId() + " already created, returning existin...");
+			log.debug("context for project: " + project.getProjectId()
+					+ " already created, returning existin...");
 			applicationContext = this.contextTable.get(project.getProjectId());
 		} else {
 			applicationContext = new ClassPathXmlApplicationContext(this.configLocation);
@@ -54,7 +63,7 @@ public class ApplicationContextFactory {
 			props.put("db_path", project.getRootPath());
 			PropertyPlaceholderConfigurer cfg = new PropertyPlaceholderConfigurer();
 			cfg.setProperties(props);
-			
+
 			log.debug("configuring context with path: " + project.getRootPath());
 			applicationContext.addBeanFactoryPostProcessor(cfg);
 			applicationContext.refresh();
@@ -63,20 +72,36 @@ public class ApplicationContextFactory {
 		}
 		return applicationContext;
 	}
-	
+
 	/**
-	 * Set the location of the spring config file to be used by the
-	 * factory to create the application contexts.
-	 * @param configLocation the location of the config file (i.e. 
-	 * beans.xml etc.)
+	 * Set the location of the spring config file to be used by the factory to
+	 * create the application contexts.
+	 * 
+	 * @param configLocation
+	 *            the location of the config file (i.e. beans.xml etc.)
 	 * @see ClassPathXmlApplicationContext
 	 */
 	public void setConfigLocation(String[] configLocation) {
-        log.debug("Setting config Location to: ");
-        for(String bla : configLocation)
-        {
-            log.debug(bla);
-        }
+		log.debug("Setting config Location to: ");
+		for (String bla : configLocation) {
+			log.debug(bla);
+		}
 		this.configLocation = configLocation;
+	}
+
+	public ILogEntryDao getLogEntryDao(Project p) {
+		return (ILogEntryDao) getApplicationContext(p).getBean("logEntryDao");
+	}
+
+	public IProjectMemberDao getProjectMemberDao(Project p) {
+		return (IProjectMemberDao) getApplicationContext(p).getBean("projectMemberDao");
+	}
+
+	public INoteObjectDao getNoteObjectDao(Project p) {
+		return (INoteObjectDao) getApplicationContext(p).getBean("noteObjectDao");
+	}
+
+	public IFileObjectDao getFileObjectDao(Project p) {
+		return (IFileObjectDao) getApplicationContext(p).getBean("fileObjectDao");
 	}
 }
