495e65c26b418aeac91c63906067b20da51189d3
simon <simon@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-19 15:48:43 +0000

    D'oh
    git-svn-id: svn://svn.jakeapp.com/trunk@1273 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ gui/src/main/java/com/jakeapp/gui/swing/exceptions/NestedException.java
@@ -0,0 +1,63 @@
+package com.jakeapp.gui.swing.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Generic exception that may nest additional exceptions.
+ * @author Simon
+ *
+ */
+public class NestedException extends Exception {
+	private static final long serialVersionUID = 5050234839403267541L;
+
+	private List<Exception> nestedExceptions;
+	{
+		this.nestedExceptions = new ArrayList<Exception>();
+	}
+	
+	public void append(Exception e) {
+		this.nestedExceptions.add(e);
+	}
+
+	/**
+	 * Get a list of nested <code>Exception</code>s.
+	 * @return list of nested <code>Exception</code>s
+	 */
+	protected List<Exception> getNestedExceptions() {
+		return this.nestedExceptions;
+	}
+
+	@Override
+	public String getMessage() {
+		String str = new String();
+		str = super.getMessage() + ", " + nestedExceptions.size() + " nested exception(s); \n";
+		
+		return str;
+	}
+	
+	/**
+	 * Get the array of <code>StackTraceElements</code>. It is a hacked bunch of all nested exceptions.
+	 * @return the array
+	 */
+	@Override
+	public StackTraceElement[] getStackTrace() {
+		//FIXME: delete that hack!
+		List<StackTraceElement> stack = new ArrayList<StackTraceElement>();
+		
+		for (StackTraceElement s : super.getStackTrace()) {
+			stack.add(s);
+		}
+		
+		for (Exception e : this.getNestedExceptions()) {
+			stack.add(new StackTraceElement("nested Exception - " + e.getMessage() + "\n", "", "", 0));
+			for (StackTraceElement s : super.getStackTrace()) {
+				stack.add(s);
+			}
+		}
+		StackTraceElement[] array = new StackTraceElement[1];
+		return stack.toArray(array);
+	}
+
+}
