f29b5868ca6163141f75b431028423531538d6f9
dominik <dominik@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-01-20 21:27:32 +0000

    I think I've did it! Please test.
    git-svn-id: svn://svn.jakeapp.com/trunk@1317 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/services/futures/AllProjectFilesFuture.java
+++ core/src/main/java/com/jakeapp/core/services/futures/AllProjectFilesFuture.java
@@ -2,18 +2,32 @@ package com.jakeapp.core.services.futures;
 
 import com.jakeapp.core.dao.IFileObjectDao;
 import com.jakeapp.core.domain.FileObject;
+import com.jakeapp.core.domain.Project;
 import com.jakeapp.core.util.availablelater.AvailableLaterObject;
 import com.jakeapp.core.util.availablelater.AvailabilityListener;
+import com.jakeapp.core.util.ProjectApplicationContextFactory;
+
 import java.util.List;
+import java.util.ArrayList;
+
 
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
 
 
 public class AllProjectFilesFuture extends
 	AvailableLaterObject<List<FileObject>> {
 
+    private static Logger log = Logger.getLogger(AllProjectFilesFuture.class);
+
 	private IFileObjectDao fileObjectDao;
-	
+
+
+    private ProjectApplicationContextFactory applicationContextFactory;
+    private Project project;
+
 	private void setFileObjectDao(IFileObjectDao fileObjectDao) {
 		this.fileObjectDao = fileObjectDao;
 	}
@@ -27,12 +41,62 @@ public class AllProjectFilesFuture extends
 		this.setFileObjectDao(dao);
 	}
 
+
+    public AllProjectFilesFuture(AvailabilityListener listener, ProjectApplicationContextFactory
+            applicationContextFactory, Project project) {
+		super(listener);
+		this.applicationContextFactory = applicationContextFactory;
+        this.project = project;
+	}
+
 	@Override
 	@Transactional
 	public void run() {
 		//FIXME simplicistic implementation
-		this.set(
-				this.getFileObjectDao().getAll()
-		);
+
+//        log.debug("\n\n\n\n\n\n\n\n\n\n\nstarting thread & running it... \n\n\n\n\n\n\n\n\n\n\n");
+
+        /**
+         *  I'M NOT QUIETE SURE WHAT EXACTLY MADE THIS WORK. PLEASE DON'T REMOVE THE COMMENTS.
+         * THAnKS, Dominik
+         */
+
+
+//        SessionFactory sf = (SessionFactory) this
+//                .applicationContextFactory.getApplicationContext(project).getBean("sessionFactory");
+
+//        if(sf.isClosed())
+//        {
+//            log.debug("sf.isClosed()");
+//        }
+//        else
+//        {
+//            log.debug("NOT sf.isClosed()");
+//        }
+//
+//        log.debug("open session");
+//        sf.openSession();
+
+        List<FileObject> result = new ArrayList<FileObject>();
+        result = this.applicationContextFactory.getFileObjectDao(project).getAll();
+
+//        for(FileObject file : result)
+//        {
+//            System.out.println("file = " + file);
+//        }
+
+        this.set(result);
+
+
+
+//        log.debug("close session");
+//        sf.close();
+//        sf = null;
+//        log.debug("\n\n\n\n\n\n\n\n\n\n\nend thread... \n\n\n\n\n\n\n\n\n\n\n");
+
+
+
+
+
 	}
 }
