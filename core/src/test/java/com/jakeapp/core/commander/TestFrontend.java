188b1ba04386a2db89f637aca0abc7ef0b9411fb
johannes <johannes@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-02-05 23:06:12 +0000

    These tests don't work and document unclear usage of the core interfaces.
    git-svn-id: svn://svn.jakeapp.com/trunk@1366 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ core/src/test/java/com/jakeapp/core/commander/TestFrontend.java
@@ -0,0 +1,74 @@
+package com.jakeapp.core.commander;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.jakeapp.core.domain.Project;
+import com.jakeapp.core.domain.ProtocolType;
+import com.jakeapp.core.domain.ServiceCredentials;
+import com.jakeapp.core.services.IFrontendService;
+import com.jakeapp.core.services.IProjectsManagingService;
+import com.jakeapp.core.services.MsgService;
+import com.jakeapp.jake.test.FSTestCommons;
+
+
+public class TestFrontend extends TmpdirEnabledTestCase {
+
+	private IFrontendService frontend;
+
+	private String sessionId;
+
+	private IProjectsManagingService pms;
+
+	private static final String id = "testuser1@my.provider";
+
+	private static final String password = "mypasswd";
+
+	@Before
+	public void setup() throws Exception {
+		super.setup();
+
+		FSTestCommons.recursiveDelete(new File(".jake"));
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				new String[] { "/com/jakeapp/core/applicationContext.xml" });
+		frontend = (IFrontendService) applicationContext.getBean("frontendService");
+
+		sessionId = frontend.authenticate(new HashMap<String, String>());
+		pms = frontend.getProjectsManagingService(sessionId);
+	}
+
+	@Test
+	@Ignore // this fails. Why? What is the right usage?
+	public void testCreateAndAssign() throws Exception {
+		ServiceCredentials cred = new ServiceCredentials(id, password);
+		cred.setProtocol(ProtocolType.XMPP);
+		MsgService msg = frontend.addAccount(sessionId, cred);
+
+		Project project = pms.createProject(tmpdir.getName(), tmpdir.getAbsolutePath(), msg);
+		Assert.assertNull(project.getUserId());
+
+		Assert.assertEquals(0, pms.getProjectMembers(project).size());
+		pms.assignUserToProject(project, msg.getUserId()); // what is the expected behaviour?
+	}
+
+	@Test
+	@Ignore // this fails. Why? What is the right usage?
+	public void testCreateAndAssignAfterwards() throws Exception {
+		Project project = pms.createProject(tmpdir.getName(), tmpdir.getAbsolutePath(), null);
+		Assert.assertNull(project.getMessageService());
+		// Assert.assertNull(project.getUserId().getUuid());
+
+		ServiceCredentials cred = new ServiceCredentials(id, password);
+		cred.setProtocol(ProtocolType.XMPP);
+		MsgService msg = frontend.addAccount(sessionId, cred);
+
+		pms.assignUserToProject(project, msg.getUserId());
+	}
+}
