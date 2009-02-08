97421f708574e2b42ff57f269fe46a9d14eb8ca4
johannes <johannes@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-02-08 03:45:21 +0000

    renamed to avoid automatic execution. added some more runs (listing)
    ASSIGNED - bug 13: announce 
    http://dev.jakeapp.com/bugs/show_bug.cgi?id=13
    
    git-svn-id: svn://svn.jakeapp.com/trunk@1378 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/test/java/com/jakeapp/core/commander/AnnounceRuns.java
+++ core/src/test/java/com/jakeapp/core/commander/AnnounceRuns.java
@@ -3,33 +3,57 @@ package com.jakeapp.core.commander;
 import java.io.File;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
-public class AnnounceTest extends TestDBEnabledTestCase {
+public class AnnounceRuns extends TestDBEnabledTestCase {
+
+	private static final String project = "testproject1";;
 
 	@Override
 	protected String getDbTemplateName() {
 		return "oneuserWithOneProjectContainingNotes";
 	}
-
-	@Test
-	public void announce() {
+	
+	@Before
+	public void setup() throws Exception {
+		super.setup();
 		String pwd = new File(".").getAbsolutePath();
 		System.out.println("You are now in " + pwd);
-		String project = "testproject1";
 		File projectdir = new File(pwd, project);
 		projectdir.mkdir();
 		Assert.assertTrue(folderExists(projectdir));
-		projectdir.getAbsolutePath();
-		
+	}
+	
+	@Test
+	public void testSetup() {
 		FifoStreamer fifo = new FifoStreamer();
 		fifo.addLine("coreLogin testuser1@localhost mypw");
-		//fifo.addLine("createProject " + project);
-		//fifo.addLine("closeProject");
 		fifo.addLine("listProjects");
 		fifo.addLine("openProject " + project);
-		//fifo.addLine("deleteProject");
+		fifo.addLine("coreLogout");
+		fifo.addLine("stop");
+		new JakeCommander(fifo);
+	}
+
+	@Test
+	public void listObjects() {
+		FifoStreamer fifo = new FifoStreamer();
+		fifo.addLine("coreLogin testuser1@localhost mypw");
+		fifo.addLine("openProject " + project);
+		fifo.addLine("listObjects");
+		fifo.addLine("coreLogout");
+		fifo.addLine("stop");
+		new JakeCommander(fifo);
+	}
+
+	@Test
+	public void announce() {
+		FifoStreamer fifo = new FifoStreamer();
+		fifo.addLine("coreLogin testuser1@localhost mypw");
+		fifo.addLine("openProject " + project);
+		fifo.addLine("listObjects");
 		fifo.addLine("coreLogout");
 		fifo.addLine("stop");
 		new JakeCommander(fifo);
