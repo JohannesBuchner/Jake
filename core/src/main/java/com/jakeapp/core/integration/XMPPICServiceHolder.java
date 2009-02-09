4a53793e933660517453de891c87580c582e77a6
dominik <dominik@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-02-09 16:51:11 +0000

    Modified JakeCommander a bit. Renamed methods (addAccount/coreLogin/coreRegister/etc.) for clarity. Moved documentation stuff to corresponding place in respect to maven documentation structure.
    git-svn-id: svn://svn.jakeapp.com/trunk@1384 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ core/src/main/java/com/jakeapp/core/integration/XMPPICServiceHolder.java
@@ -0,0 +1,125 @@
+package com.jakeapp.core.integration;
+
+import com.jakeapp.jake.ics.impl.xmpp.XmppICService;
+import com.jakeapp.jake.ics.impl.xmpp.XmppUserId;
+import com.jakeapp.jake.ics.ICService;
+import com.jakeapp.jake.ics.filetransfer.methods.ITransferMethodFactory;
+import com.jakeapp.jake.ics.users.IUsersService;
+import com.jakeapp.jake.ics.msgservice.IMsgService;
+import com.jakeapp.jake.ics.status.IStatusService;
+import com.jakeapp.core.domain.ServiceCredentials;
+import com.jakeapp.core.domain.Project;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * Integration class to integrate Multi-Connect XMPPICService with core.
+ */
+public final class XMPPICServiceHolder {
+
+    public static final String namespace = "http://jakeapp.com/protocols/xmpp/versions/1";
+    public static final String defaultGroupId = "Jake";
+
+
+    // Key: UserId String
+    private static final Map<String, ServiceCredentials> user_cred = new HashMap<String, ServiceCredentials>();
+
+    // key: UserId String
+    private static final Map<String, XmppICService> user_service = new HashMap<String, XmppICService>();
+
+
+
+    // key: Project UUID
+    private static final Map<UUID, String> project_user = new HashMap<UUID, String>();
+
+
+
+
+    private ServiceCredentials credentials;
+
+    private String groupName;
+
+    public XMPPICServiceHolder() {
+
+    }
+
+
+    public XMPPICServiceHolder getXmppService(final ServiceCredentials credentials) {
+//        this.credentials = credentials;
+
+
+
+
+
+
+
+        if (credentials.getUserId() != null) {
+            if (!user_cred.containsKey(credentials.getUserId())) {
+                user_cred.put(credentials.getUserId(), credentials);
+            } else {
+                if(!user_service.containsKey(credentials.getUserId()))
+                {
+                    user_cred.put(credentials.getUserId(), credentials);
+                }
+                else
+                {
+                    if(user_cred.get(credentials.getUserId()).equals(credentials))
+                    {
+                        // do nothing
+                    }
+                    else
+                    {
+                        // TODO throw new Exception; Modifying an already logged in user!
+                    }
+                }
+            }
+         // create xmpp instance
+            return this;
+        }
+        else
+        {
+            // todo throw new Exception;
+            return null;
+        }
+    }
+
+
+    public XMPPICServiceHolder getXmppService(final Project project)
+    {
+
+
+        return null;
+    }
+
+
+    public boolean login() {
+        if (this.credentials == null)
+            return false;
+
+
+        if (!user_cred.containsKey(credentials.getUserId()))
+            return false;
+
+
+        XmppICService service = user_service.get(credentials.getUserId());
+
+        if(service == null)
+            service = new XmppICService(namespace, this.groupName);
+
+
+
+
+
+//            service.getStatusService().login(
+//
+//                    new XmppUserId(credentials.getUserId()),
+//                    credentials.getPlainTextPassword());
+
+
+        return false;
+
+    }
+
+}
