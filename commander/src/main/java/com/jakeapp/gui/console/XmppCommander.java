569cf43ee4c2b3f473feef7bf78cb310a0ef058c
johannes <johannes@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-02-24 20:26:07 +0000

    begin of jake mock counterpart
    git-svn-id: svn://svn.jakeapp.com/trunk@1541 cfed719c-3465-4289-a5de-482d64f03f8e

--- commander/src/main/java/com/jakeapp/gui/console/XmppCommander.java
+++ commander/src/main/java/com/jakeapp/gui/console/XmppCommander.java
@@ -1,5 +1,15 @@
 package com.jakeapp.gui.console;
 
+import java.io.IOException;
+
+import com.jakeapp.core.services.XMPPMsgService;
+import com.jakeapp.gui.console.commandline.LazyCommand;
+import com.jakeapp.jake.ics.UserId;
+import com.jakeapp.jake.ics.exceptions.NetworkException;
+import com.jakeapp.jake.ics.exceptions.TimeoutException;
+import com.jakeapp.jake.ics.impl.xmpp.XmppICService;
+import com.jakeapp.jake.ics.impl.xmpp.XmppUserId;
+
 
 /**
  * Test client accepting cli input
@@ -10,14 +20,87 @@ public class XmppCommander extends Commander {
 		super(args);
 	}
 
+	public static void main(String[] args) {
+		new XmppCommander(args);
+	}
+
 	@Override
 	protected void onShutdown() {
-		
+		//
 	}
 
 	@Override
 	protected void onStartup() {
-		
+		//
+	}
+
+	private XmppICService ics = null;
+
+	class Login extends LazyCommand {
+
+		public Login() {
+			super("login", "login <xmppid> <passwd> <groupname>",
+					"xmppid = user@host/resource where resource is 'Jake' or the "
+							+ "projectid, groupname = projectname (group in roster)");
+		}
+
+		@Override
+		public boolean handleArguments(String[] args) {
+			if (args.length != 4)
+				return false;
+			XmppUserId id = new XmppUserId(args[1]);
+			XmppCommander.this.ics = new XmppICService(XMPPMsgService.namespace, args[3]);
+			try {
+				XmppCommander.this.ics.getStatusService().login(id, args[2]);
+			} catch (TimeoutException e) {
+				e.printStackTrace();
+			} catch (NetworkException e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
+
+	}
+
+	class Logout extends LazyCommand {
+
+		public Logout() {
+			super("logout", "logout", "groupname = projectname (group in roster)");
+		}
+
+		@Override
+		public boolean handleArguments(String[] args) {
+			try {
+				XmppCommander.this.ics.getStatusService().logout();
+			} catch (TimeoutException e) {
+				e.printStackTrace();
+			} catch (NetworkException e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
+	}
+
+	class ListUsers extends LazyCommand {
+
+		public ListUsers() {
+			super("listUsers");
+		}
+
+		@Override
+		public boolean handleArguments(String[] args) {
+			try {
+				for (UserId u : XmppCommander.this.ics.getUsersService().getUsers()) {
+					System.out.println(u + " - capable? "
+							+ XmppCommander.this.ics.getUsersService().isCapable(u));
+				}
+			} catch (NetworkException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
 	}
 
 }
