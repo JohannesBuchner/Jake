59c5be5d9eecb8ab12a83fd4be595b065ffc6fca
chris <chris@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-02-28 14:04:10 +0000

    checking in poke run for testing of weird bug
    git-svn-id: svn://svn.jakeapp.com/trunk@1608 cfed719c-3465-4289-a5de-482d64f03f8e

---
+++ commander/src/test/java/com/jakeapp/gui/console/PrimitivePokeRun.java
@@ -0,0 +1,41 @@
+package com.jakeapp.gui.console;
+
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.jakeapp.jake.test.FSTestCommons;
+import com.jakeapp.jake.test.TmpdirEnabledTestCase;
+import com.jakeapp.gui.console.JakeCommander;
+
+
+/**
+ * This class is (atm) for running manually in the IDE and not for inclusion in
+ * build automation
+ *
+ * @author johannes
+ */
+public class PrimitivePokeRun extends TmpdirEnabledTestCase {
+
+	@Override
+	@Before
+	public void setup() throws Exception {
+		FSTestCommons.recursiveDelete(new File(".jake"));
+		super.setup();
+	}
+
+	@Test
+	public void bugRun() {
+		FifoStreamer fifo = new FifoStreamer();
+		fifo.addLine("coreLogin john@localhost jake");
+		fifo.addLine("login");
+		fifo.addLine("openProject c:\\jake\\test3");
+		fifo.addLine("startProject c:\\jake\\test3");
+		fifo.addLine("poke john@localhost");
+		fifo.addLine("coreLogout");
+		fifo.addLine("stop");
+		new JakeCommander(fifo);
+	}
+}
