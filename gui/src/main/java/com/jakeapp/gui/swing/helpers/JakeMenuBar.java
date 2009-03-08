7999e24c88733e4d9fc376fa2eca96aa75ab4de5
peter <peter@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-03-08 22:30:06 +0000

    menu fixes
    git-svn-id: svn://svn.jakeapp.com/trunk@1854 cfed719c-3465-4289-a5de-482d64f03f8e

--- gui/src/main/java/com/jakeapp/gui/swing/helpers/JakeMenuBar.java
+++ gui/src/main/java/com/jakeapp/gui/swing/helpers/JakeMenuBar.java
@@ -4,24 +4,24 @@ import ch.randelshofer.quaqua.JSheet;
 import com.jakeapp.core.domain.Project;
 import com.jakeapp.gui.swing.JakeMainApp;
 import com.jakeapp.gui.swing.JakeMainView;
-import com.jakeapp.gui.swing.globals.JakeContext;
+import com.jakeapp.gui.swing.actions.file.*;
+import com.jakeapp.gui.swing.actions.notes.CommitNoteAction;
+import com.jakeapp.gui.swing.actions.notes.CreateNoteAction;
+import com.jakeapp.gui.swing.actions.notes.DeleteNoteAction;
+import com.jakeapp.gui.swing.actions.notes.SoftlockNoteAction;
+import com.jakeapp.gui.swing.actions.project.CreateProjectAction;
+import com.jakeapp.gui.swing.actions.project.DeleteProjectAction;
+import com.jakeapp.gui.swing.actions.project.RenameProjectAction;
+import com.jakeapp.gui.swing.actions.project.StartStopProjectAction;
+import com.jakeapp.gui.swing.actions.project.SyncProjectAction;
 import com.jakeapp.gui.swing.actions.users.InviteUsersAction;
 import com.jakeapp.gui.swing.actions.view.SwitchFilesProjectContextAction;
 import com.jakeapp.gui.swing.actions.view.SwitchLoginProjectContextAction;
 import com.jakeapp.gui.swing.actions.view.SwitchNewsProjectContextAction;
 import com.jakeapp.gui.swing.actions.view.SwitchNotesProjectContextAction;
-import com.jakeapp.gui.swing.actions.notes.CommitNoteAction;
-import com.jakeapp.gui.swing.actions.notes.SoftlockNoteAction;
-import com.jakeapp.gui.swing.actions.notes.CreateNoteAction;
-import com.jakeapp.gui.swing.actions.notes.DeleteNoteAction;
-import com.jakeapp.gui.swing.actions.project.CreateProjectAction;
-import com.jakeapp.gui.swing.actions.project.StartStopProjectAction;
-import com.jakeapp.gui.swing.actions.project.RenameProjectAction;
-import com.jakeapp.gui.swing.actions.project.DeleteProjectAction;
-import com.jakeapp.gui.swing.actions.project.SyncProjectAction;
-import com.jakeapp.gui.swing.actions.file.*;
 import com.jakeapp.gui.swing.dialogs.debugging.ActiveTasks;
 import com.jakeapp.gui.swing.dialogs.debugging.JakeDebugger;
+import com.jakeapp.gui.swing.globals.JakeContext;
 import com.jakeapp.gui.swing.panels.FilePanel;
 import com.jakeapp.gui.swing.xcore.CreateExampleProject;
 import com.jakeapp.gui.swing.xcore.EventCore;
@@ -72,8 +72,6 @@ public class JakeMenuBar extends JMenuBar {
 		projectMenu.add(new JMenuItem(new DeleteProjectAction()));
 		projectMenu.addSeparator();
 		projectMenu.add(new JMenuItem(new InviteUsersAction(true)));
-		projectMenu.addSeparator();
-		// TODO: sign in action
 
 		this.add(projectMenu);
 
@@ -151,9 +149,9 @@ public class JakeMenuBar extends JMenuBar {
 								.setApplicationIcon(UIManager.getIcon("OptionPane.informationIcon"));
 				aboutFrame.setBuildVersion("001");
 				aboutFrame.setCopyright("Copyright 2007-2009, Best ASE Team TU Vienna");
-				aboutFrame
-								.setCredits("<html><body>Jake<br>" + "<a href=\"http://jakeapp.com/\">jakeapp.com</a><br>" + "<br>We are proud to present you Jake." + "<b></b><br>" + "Send your Feedback to: " + "<a href=\"mailto:jake@jakeapp.com\">jake@jakeapp.com</a>" + "</body></html>",
-												"text/html");
+				aboutFrame.setCredits(
+								"<html><body>Jake<br>" + "<a href=\"http://jakeapp.com/\">jakeapp.com</a><br>" + "<br>We are proud to present you Jake." + "<b></b><br>" + "Send your Feedback to: " + "<a href=\"mailto:jake@jakeapp.com\">jake@jakeapp.com</a>" + "</body></html>",
+								"text/html");
 				aboutFrame.setHyperlinkListener(new HyperlinkListener() {
 
 					public void hyperlinkUpdate(HyperlinkEvent e) {
@@ -345,16 +343,17 @@ public class JakeMenuBar extends JMenuBar {
 				quit();
 			}
 		});
-		if (!QuitJMenuItem.isAutomaticallyPresent())
+		if (!QuitJMenuItem.isAutomaticallyPresent()) {
 			projectMenu.addSeparator();
-		projectMenu.add(quitMenuItem);
+			projectMenu.add(quitMenuItem);
+		}
 
 		// add special mac os event listener
 		if (Platform.isMac()) {
 			MRJAdapter.addReopenApplicationListener(new ActionListener() {
 
 				public void actionPerformed(ActionEvent e) {
-					log.debug("reopen");
+					log.trace("reopen");
 					JakeMainView.setMainWindowVisible(true);
 				}
 			});
@@ -362,7 +361,7 @@ public class JakeMenuBar extends JMenuBar {
 			MRJAdapter.addOpenDocumentListener(new ActionListener() {
 
 				public void actionPerformed(ActionEvent e) {
-					log.debug("openDocument");
+					log.trace("openDocument");
 					// TODO
 				}
 			});
@@ -374,7 +373,7 @@ public class JakeMenuBar extends JMenuBar {
 
 
 	private void quit() {
-		log.debug("calling quit from MenuBar");
+		log.trace("calling quit from MenuBar");
 		JakeMainView.getMainView().quit();
 	}
 }
