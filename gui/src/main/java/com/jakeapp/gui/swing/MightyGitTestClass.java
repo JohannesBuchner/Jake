6da1c7c28e521f242829771760934b84ecd4b2d6
Peter Steinberger <studpete@ps-macpro.local>
2009-03-17 16:36:07 +0100

    git test

---
+++ gui/src/main/java/com/jakeapp/gui/swing/MightyGitTestClass.java
@@ -0,0 +1,9 @@
+package com.jakeapp.gui.swing;
+
+/**
+ * This Class does nothing except testing id the git bridge works
+ * There will be a soft migration, both svn and git work happily together =)
+ * @author studpete
+ */
+public class MightyGitTestClass {
+}
