a79b484358bcb83b62e24988bb5f3f00a7fa54ac
dominik <dominik@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-03-21 14:51:08 +0000

    Added documentation to ILogable and Invitation. Marked InvitationState as deprecated.
    git-svn-id: svn://svn.jakeapp.com/trunk@2020 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/domain/ILogable.java
+++ core/src/main/java/com/jakeapp/core/domain/ILogable.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 /**
  * This interface is used within logEntrys.
- * Every object which should be able to be put into &quot;belongsTo&quot; has
+ * Every object which should be able to be put into <code>LogEntry.belongsTo</code> has
  * to implement this interface.
  * @see com.jakeapp.core.domain.logentries.LogEntry#getBelongsTo()
  */
