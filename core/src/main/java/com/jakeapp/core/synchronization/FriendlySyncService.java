4d1f8a16d1366fe8f30188f680cebad5fb7f9a31
christopher <christopher@cfed719c-3465-4289-a5de-482d64f03f8e>
2009-03-24 15:51:29 +0000

    Comments for com.jakeapp.core.synchronization
    git-svn-id: svn://svn.jakeapp.com/trunk@2145 cfed719c-3465-4289-a5de-482d64f03f8e

--- core/src/main/java/com/jakeapp/core/synchronization/FriendlySyncService.java
+++ core/src/main/java/com/jakeapp/core/synchronization/FriendlySyncService.java
@@ -15,14 +15,25 @@ import com.jakeapp.jake.ics.exceptions.NotLoggedInException;
 
 /**
  * Provides loops for {@link ISyncService} Only loops belong here, add
- * functionality in {@link ISyncService}
+ * functionality in {@link ISyncService}. The class is only for convenience methods.
  * 
  * @author johannes
  */
 public abstract class FriendlySyncService implements IFriendlySyncService {
 
+	/**
+	 * @param project A Project to get all members for.
+	 * @return An iterable of all users that are in the project, including the current user
+	 * 	if they are in the project.
+	 * @throws NoSuchProjectException if <code>project</code> is null or does not exist.
+	 */
 	abstract protected Iterable<User> getProjectMembers(Project project) throws NoSuchProjectException;
 
+	/*
+	 * starts the logsync with all members of the project.
+	 * @see com.jakeapp.core.synchronization.IFriendlySyncService#startLogSync(
+	 * 	com.jakeapp.core.domain.Project)
+	 */
 	public Map<User, Iterable<LogEntry<ILogable>>> startLogSync(Project project)
 			throws IllegalArgumentException, IllegalProtocolException, NoSuchProjectException {
 		Map<User, Iterable<LogEntry<ILogable>>> lm = new HashMap<User, Iterable<LogEntry<ILogable>>>();
@@ -36,7 +47,7 @@ public abstract class FriendlySyncService implements IFriendlySyncService {
 	 * (non-Javadoc)
 	 * 
 	 * @see
-	 * com.jakeapp.core.synchronization.IFriendlySyncService#poke(com.jakeapp
+	 * com.jakeapp.core.synchronization.ISyncService#poke(com.jakeapp
 	 * .core.domain.Project)
 	 */
 	public void poke(Project project) throws NoSuchProjectException {
