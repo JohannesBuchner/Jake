5e8f935e529ca47ac78c1e05f059e69862303d22
Johannes Buchner <buchner.johannes@gmx.at>
2009-11-15 20:53:45 +1300

    seperated ics* too

--- ics/src/main/java/com/jakeapp/jake/ics/exceptions/OtherUserOfflineException.java
+++
@@ -1,9 +0,0 @@
-package com.jakeapp.jake.ics.exceptions;
-
-/**
- * @author johannes
- * The operation wanted to talk to another user, who has gone offline
- */
-@SuppressWarnings("serial")
-public class OtherUserOfflineException extends Exception {
-}
